291b4ce02703e121a89fe125c176ae1d7adb8b25
Sahil7903 <158484462+Sahil7903@users.noreply.github.com>
2024-03-15 21:35:15 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+let eduButton = document.getElementById("educationButton");
+let eduButtonQualification = document.getElementById("educationButtonQualification");
+let eduButtonHobby = document.getElementById("educationButtonHobby");
+let qDescription = document.getElementById("qualificationDescription");
+
+eduButtonHobby.textContent="Hello Buddy";
+
+function qualification() {
+    qDescription.textContent = "Hello I've skills on sleeping";
+}
